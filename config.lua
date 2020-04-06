9433af8be4597c9c68862f84aa0f961a5aa20c6f
Im-Sam <53315422+Im-Sam@users.noreply.github.com>
2020-04-06 02:38:57 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.SellDistance = 2.0 -- The distance before you can press e and negotiate.
+Config.DiscussTime = 2500 -- Seconds of the ped waiting for answer of purchase or not.
+
+Config.DrugItems = { "coke", "meth", "lsd", "heroin" } -- The items you sell.
+Config.Payment = { 950, 1350 } -- This declares what the math.random(1st, 2nd) is.
